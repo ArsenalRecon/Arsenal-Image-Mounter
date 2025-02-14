82c11273458b2e12a6d1fcec4ab5576bbf4d0b36
LTRData <ol@ltr-data.se>
2025-02-14 11:53:15 +0100

    Copy native libs to PS publish dir

--- Managed Source/publishps.cmd
+++ Managed Source/publishps.cmd
@@ -1,1 +1,11 @@
-for %%a in (net48 net8.0 net9.0) do dotnet publish -c Release -f %%a -o publish\PowerShell\%%a Arsenal.ImageMounter.PowerShell
+
+for %%a in (net48 net8.0 net9.0) do (
+  dotnet publish -c Release -f %%a -o publish\PowerShell\%%a Arsenal.ImageMounter.PowerShell
+  robocopy /MIR ..\lib publish\PowerShell\%%a\lib
+)
+
+pushd publish
+
+7z a -r AIM_PowerShell.7z PowerShell
+
+popd
