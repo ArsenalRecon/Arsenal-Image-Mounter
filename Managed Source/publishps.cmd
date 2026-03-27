3edbe07e392eb34d87ef2ab9608f92a3417ea8ad
LTRData <ol@ltr-data.se>
2026-03-27 22:44:53 +0100

    Managed API 3.12.344

--- Managed Source/publishps.cmd
+++ Managed Source/publishps.cmd
@@ -2,7 +2,7 @@ dotnet restore
 
 rd /s /q publish\PowerShell
 
-for %%a in (net48 net8.0 net9.0) do (
+for %%a in (net48 net8.0 net9.0 net10.0) do (
   dotnet publish --no-restore -c Release -f %%a -o publish\PowerShell\%%a Arsenal.ImageMounter.PowerShell
   robocopy /MIR ..\lib publish\PowerShell\%%a\lib
 )
